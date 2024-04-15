530e52c6b20ab3d030b9d77fe6a07735a8aadb59
Dylan Burnham <dburnham1212@gmail.com>
2024-04-15 17:32:57 -0400

    setting up user router and adding toastr to frontend

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+const USER_KEY = 'User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
---
+++ frontend/src/app/shared/models/User.ts
@@ -0,0 +1,6 @@
+export class Uuser{
+  id!: string;
+  username!: string;
+  email!: string;
+  token!: string;
+}
